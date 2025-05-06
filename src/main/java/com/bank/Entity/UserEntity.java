db1275ce76281ffafd30cb195720bda6c541a3a2
saikumar2001-0304 <saikumarmarella29@gmail.com>
2025-05-06 23:23:03 +0530

    added user entity

---
+++ src/main/java/com/bank/Entity/UserEntity.java
@@ -0,0 +1,20 @@
+package com.bank.Entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class UserEntity {
+
+	private Long userId;
+	private String userName;
+	private String password;
+	private String email;
+	//optional for notifications
+	private String phoneNumber;
+	//take enum also
+	private String role;
+	private String status;
+	//createdAt	Timestamp	Account creation timestamp
+	
+	
+}
